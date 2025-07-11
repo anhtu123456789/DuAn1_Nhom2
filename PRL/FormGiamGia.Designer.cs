faede6b99e9389d958c418001288e1391fca2582
TU ANH <tutaph50519@gmail.com>
2025-07-11 09:45:55 +0700

    Add project files.

--- PRL/FormGiamGia.Designer.cs
+++ PRL/FormGiamGia.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace PRL.Properties
+﻿namespace PRL
 {
-    partial class FormDoanhThu
+    partial class FormGiamGia
     {
         /// <summary>
         /// Required designer variable.
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormDoanhThu";
+            this.Text = "FormGiamGia";
         }
 
         #endregion
--- PRL/FormGiamGia.cs
+++ PRL/FormGiamGia.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace PRL.Properties
+namespace PRL
 {
-    public partial class FormDoanhThu : Form
+    public partial class FormGiamGia : Form
     {
-        public FormDoanhThu()
+        public FormGiamGia()
         {
             InitializeComponent();
         }
