8238092462f670de281c083f3d09fb0c45969b89
TU ANH <tutaph50519@gmail.com>
2025-07-11 09:55:10 +0700

    sửa

--- DuAn1_Nhom2/Model/DoanhThu.cs
+++ DuAn1_Nhom2/Model/DoanhThu.cs
@@ -19,4 +19,4 @@ namespace DAL.Model
         public virtual HoaDon? HoaDon { get; set; }
     }
 }
-// This code defines a partial class for DoanhThu, which represents a revenue record in the system.
+// This code defines a partial class f
